5a7c840c97a844e572e263e5580fd9543e5b25b9
mid-kid <esteve.varela@gmail.com>
2020-07-15 23:03:00 +0200

    Add simple hex functions for C

---
+++ c/hex.c
@@ -0,0 +1,42 @@
+void hex_read(const unsigned char *buf, const unsigned len)
+{
+    for (unsigned i = 0; i < len; i += 0x10) {
+        printf("   ");
+        for (unsigned x = i; x < i + 0x10 && x < len; x++)  {
+            printf(" %02X", buf[x]);
+        }
+        printf("\n");
+    }
+}
+
+unsigned hex_dump(unsigned char *buf, const unsigned len, const char *str)
+{
+    unsigned cur = 0;
+    while (*str) {
+        if (cur >= len) return 0;
+
+        if (*str == ' ') {
+            str++;
+            continue;
+        }
+
+        unsigned char val = 0;
+        for (int i = 0; i < 2; i++) {
+            char c = str[i];
+            unsigned char v;
+            if (c >= '0' && c <= '9') {
+                v = c - '0';
+            } else if (c >= 'A' && c <= 'F') {
+                v = c - 'A' + 10;
+            } else if (c >= 'a' && c <= 'f') {
+                v = c - 'a' + 10;
+            } else {
+                return 0;
+            }
+            val |= v << (4 * (1 - i));
+        }
+        buf[cur++] = val;
+        str += 2;
+    }
+    return cur;
+}
---
+++ c/hex.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void hex_read(const unsigned char *buf, const unsigned len);
+unsigned hex_dump(unsigned char *buf, const unsigned len, const char *str);
