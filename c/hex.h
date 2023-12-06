1d9ff3697a696908f6a9c7aff0e332c8140df971
mid-kid <esteve.varela@gmail.com>
2023-12-06 11:21:54 +0000

    Unconst a bit

--- c/hex.h
+++ c/hex.h
@@ -1,4 +1,4 @@
 #pragma once
 
-void hex_dump(const unsigned char *buf, const unsigned len);
-unsigned hex_read(unsigned char *buf, const unsigned len, const char *str);
+void hex_dump(const unsigned char *buf, unsigned len);
+unsigned hex_read(unsigned char *buf, unsigned len, const char *str);
